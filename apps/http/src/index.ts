76587338122acce133d543867d2b7b23a70f0591
Tanishq Patil <tanishqp101204@gmail.com>
2025-09-02 18:29:44 +0000

    routes added

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
-import {CreateUserSchema} from "@repo/zod/types"
+import {CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/zod/types"
 
 import {prismaClient} from "@repo/db/client"
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { config } from "@repo/config/config";
+import { CustomRequest, middleware } from "./middleware.js";
 
 
 
@@ -12,7 +15,7 @@ app.use(express.json());
 
 
 
-app.get("/signup" , async(req , res) => {
+app.post("/signup" , async(req , res) => {
     const parsedData = CreateUserSchema.safeParse(req.body);
     if(!parsedData.success){
         res.json({
@@ -27,7 +30,7 @@ app.get("/signup" , async(req , res) => {
             data : {
                 email : parsedData.data.username,
                 password : hashedPassword,
-                name : parsedData.data.username
+                name : parsedData.data.name
             }
         })
 
@@ -42,6 +45,132 @@ app.get("/signup" , async(req , res) => {
     }
 
 })
+
+app.post("/signin" , async (req , res) => {
+    const parsedData = SigninSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({
+            message : "Invalid Inputs"
+        })
+        return;
+    }
+    try{
+        const user = await prismaClient.user.findFirst({
+            where : {
+                email : parsedData.data.username
+             
+            }
+        })
+        if(!user || !user.password){
+            res.status(401).json({
+                message : "user not found , invalid"
+            })
+            return;
+        }
+        const isValidPassword = await bcrypt.compare( parsedData.data.password , user?.password);
+        if(isValidPassword){
+            const token = jwt.sign({
+                userId : user?.id
+            } , config.SECRET_KEY)
+
+            res.json({
+                token : token
+            })
+            
+        }
+        else{
+            res.status(401).json({
+                message : "Invalid credentials"
+            })
+        }
+    }
+    catch(e){
+        res.status(411).json({
+            message : "Error while signing in"
+        })
+    }
+})
+
+app.post("/rooms" , middleware ,  async (req : CustomRequest , res) => {
+    const parsedData = CreateRoomSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({
+            message : "Incorrect data"
+        })
+        return;
+    }
+    const userId = req.userId;
+    if(!userId){
+        res.status(403).json({
+            message : "Unauthorized"
+        })
+        return;
+    }
+    try{
+        const room = await prismaClient.room.create({
+            data : {
+                slug : parsedData.data.name,
+                adminId : userId
+            }
+        })
+        res.json({
+            roomId : room.id
+        })
+    }
+    catch(e){
+        res.status(411).json({
+            message : "Room already exists or Error creating room"
+        })
+    }
+
+
+})
+
+app.get("/chats/:roomId" , middleware , async(req , res) => {
+    try{
+        const roomId = Number(req.params.roomId);
+    const messages = await prismaClient.chat.findMany({
+        where : {
+            roomId : roomId
+        },
+        orderBy : {
+            id : "desc"
+        },
+        take : 50
+    })
+
+    res.json({
+        messages : messages
+    })
+    }
+    catch(e){
+        res.status(411).json({
+            message : []
+        })
+    }
+}
+)
+
+app.get("/room/:slug", middleware, async(req, res)=>{
+    const slug=req.params.slug;
+    try{
+        const room= await prismaClient.room.findFirst({
+            where: {slug}
+        });
+        res.json({
+            message: room
+        })
+    }catch(e){
+        res.status(411).json({
+            message: "Error getting slug"
+        })
+    }
+})
+
+
+
+
+
 
 app.listen(3000);
 
--- packages/zod/src/types.ts
+++ packages/zod/src/types.ts
@@ -1,13 +1,13 @@
 import {z} from "zod";
 
 export const CreateUserSchema = z.object({
-    username : z.string().min(2).max(15),
-    password : z.string().min(4),
+    username : z.string().min(2).max(50),
+    password : z.string().min(4).max(8),
     name : z.string()
 })
 
 export const SigninSchema=z.object({
-    username:z.string().min(2).max(15),
+    username:z.string().min(2).max(50),
     password: z.string().min(4),    
 })
 
